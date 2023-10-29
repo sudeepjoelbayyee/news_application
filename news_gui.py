6d220da5b3be65eb66aae92f5d1ada50f2dbaadc
Sudeep Joel Bayye <119824811+sudeepjoelbayyee@users.noreply.github.com>
2023-10-29 06:05:12 +0530

    Update news_gui.py

--- news_gui.py
+++ news_gui.py
@@ -1,10 +1,5 @@
 import matplotlib
 matplotlib.use('Agg')
-import os
-
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
     
 import io
 import requests
